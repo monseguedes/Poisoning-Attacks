2f1f2f394e2152f0d52b44476adac33b69d77f2f
Monse Guedes Ayala <s1553571@ed.ac.uk>
2023-05-02 17:44:12 +0100

    Add fake data

--- programs/minlp/main.py
+++ programs/minlp/main.py
@@ -15,7 +15,7 @@ import flipping_attack
 
 config = {
     # Dataset
-    "dataset_name": "5num5cat",
+    "dataset_name": "10num10cat",
     "training_samples": 100,
     "poison_rate": 4,
     "seed": 3,
@@ -40,6 +40,7 @@ config = {
     "iterative_attack_n_epochs": 1,
     # Flipping attack
     "flipping_attack_n_epochs": 5,
+    "plot": False,
     # Solutions
     "datatype": "test",
 }
