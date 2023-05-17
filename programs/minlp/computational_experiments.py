cbb7605cf5f68c30c242eecb7648d60f0d7f07b6
Monse Guedes Ayala <s1553571@ed.ac.uk>
2023-05-17 14:54:17 +0100

    Update computational experiment file with dictionaries, missing saving

--- programs/minlp/computational_experiments.py
+++ programs/minlp/computational_experiments.py
@@ -21,7 +21,7 @@ def run(runs, config):
     None.
 
     """
-    config["return_benchmark"] = True
+
     folder_name = f"{runs}_BS{config['numerical_attack_mini_batch_size']}_TS{config['training_samples']}_PR{config['poison_rate']}"
     # Create directory to store results
     isExist = os.path.exists(f"results/{config['dataset_name']}/{folder_name}")
@@ -31,41 +31,66 @@ def run(runs, config):
     # Create dictionary to store results
     results = {}
     benchmark_results = {}
+
+    results["mse_per_iteration"] = []
+    results["mse_final"] = []
+    results["computational_time_per_iteration"] = []
+    results["computational_time_final"] = []
+    results["benchmark_improvement"] = []
+    results["ridge_improvement"] = []
+    benchmark_results["benchmark_mse_final"] = []
+    benchmark_results["benchmark_computational_time"] = []
+
     for run in range(runs):
         config["seed"] = run
         instance_data = instance_data_class.InstanceData(config)
+        regression_parameters = ridge_regression.run_just_training(
+            instance_data, config
+        )
         model = None
+
         # Run flipping attack
         (
             _,
             instance_data,
-            regression_parameters,
-            benchmark_solution,
+            solutions,
         ) = flipping_attack.run(config, instance_data, model)
-        improvement = (
-            (regression_parameters["mse"] - benchmark_solution["mse"])
-            / benchmark_solution["mse"]
+
+        results["mse_per_iteration"].append(solutions["mse_per_iteration"])
+        results["mse_final"].append(solutions["mse_final"])
+        results["computational_time_per_iteration"].append(
+            solutions["computational_time_per_iteration"]
+        )
+        results["computational_time_final"].append(
+            solutions["computational_time_final"]
+        )
+
+        benchmark_improvement = (
+            (solutions["mse"] - solutions["benchmark_mse_final"])
+            / solutions["benchmark_mse_final"]
             * 100
         )
-        results[run] = {"mse": regression_parameters["mse"], "improvement": improvement}
-        benchmark_results[run] = {"mse": benchmark_solution["mse"]}
+        ridge_improvement = (
+            (solutions["mse"] - regression_parameters["mse"])
+            / regression_parameters["mse"]
+            * 100
+        )
+
+        results["benchmark_improvement"].append(benchmark_improvement)
+        results["ridge_improvement"].append(solutions["ridge_improvement"])
+
+        benchmark_results["benchmark_mse_final"].append(
+            solutions["benchmark_mse_final"]
+        )
+        benchmark_results["benchmark_computational_time"].append(
+            solutions["benchmark_computational_time"]
+        )
 
     # Add average MSE to results
-    results["average"] = np.mean([results[run]["mse"] for run in range(runs)])
-    results["average_improvement"] = np.mean(
-        [results[run]["improvement"] for run in range(runs)]
-    )
-    benchmark_results["average"] = np.mean(
-        [benchmark_results[run]["mse"] for run in range(runs)]
-    )
-    # Create dataframe from results dictionary
-    results_dataframe = pd.DataFrame.from_dict(results)
-    print(results_dataframe)
-    benchmark_results_dataframe = pd.DataFrame.from_dict(benchmark_results)
-    # Save results to csv file
-    results_dataframe.to_csv(
-        f"results/{config['dataset_name']}/{folder_name}/results.csv"
-    )
-    benchmark_results_dataframe.to_csv(
-        f"results/{config['dataset_name']}/{folder_name}/benchmark_results.csv"
+    results["average_mse"] = np.mean(results["mse_final"])
+    results["average_computational_time"] = np.mean(results["computational_time_final"])
+    results["benchmark_average_improvement"] = np.mean(results["benchmark_improvement"])
+    benchmark_results["average_mse"] = np.mean(benchmark_results["mse_final"])
+    benchmark_results["average_computational_time"] = np.mean(
+        benchmark_results["benchmark_computational_time"]
     )
