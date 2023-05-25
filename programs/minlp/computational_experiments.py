a034eade052512fc4c6545df43cc65ff2cfb4d84
Monse Guedes Ayala <s1553571@ed.ac.uk>
2023-05-25 22:12:35 +0100

    Clean computational experiments

--- programs/minlp/computational_experiments.py
+++ programs/minlp/computational_experiments.py
@@ -23,6 +23,8 @@ def run(runs, config):
     ----------
     runs : int
         Number of runs to perform.
+    config : dict
+        Dictionary containing configuration parameters.
 
     Returns
     -------
@@ -30,36 +32,17 @@ def run(runs, config):
 
     """
 
-    folder_name = f"{runs}_BS{config['numerical_attack_mini_batch_size']}_TS{config['training_samples']}_PR{config['poison_rate']}"
     # Create directory to store results
+    folder_name = f"{runs}_BS{config['numerical_attack_mini_batch_size']}_TS{config['training_samples']}_PR{config['poison_rate']}_lambda{config['regularization']}"
     isExist = os.path.exists(f"results/{config['dataset_name']}/{folder_name}")
     if not isExist:
         os.makedirs(f"results/{config['dataset_name']}/{folder_name}")
 
-    # Create dictionary to store results
-    flipping_results = {}
-    benchmark_results = {}
-    unpoisoned_results = {}
-    binary_results = {}
-
-    flipping_results["mse_per_iteration"] = []
-    flipping_results["mse_final"] = []
-    flipping_results["computational_time_per_iteration"] = []
-    flipping_results["computational_time_final"] = []
-    flipping_results["benchmark_improvement"] = []
-    flipping_results["ridge_improvement"] = []
-    flipping_results["solutions_dict"] = []
-
-    benchmark_results["mse_final"] = []
-    benchmark_results["computational_time_final"] = []
-    benchmark_results["solutions_dict"] = []
-
-    unpoisoned_results["mse_final"] = []
-    unpoisoned_results["solutions_dict"] = []
-
-    binary_results["mse_final"] = []
-    binary_results["computational_time_final"] = []
-    binary_results["solutions_dict"] = []
+    # Create dictionaries to store results
+    flipping_results = collections.defaultdict(list)
+    benchmark_results = collections.defaultdict(list)
+    unpoisoned_results = collections.defaultdict(list)
+    binary_results = collections.defaultdict(list)
 
     # TODO fix not build model for each run
 
@@ -68,75 +51,52 @@ def run(runs, config):
         config["seed"] = run
         instance_data = instance_data_class.InstanceData(config)
         model = pyomo_model.PyomoModel(instance_data, config)
+
+        ## Unpoisoned model
         regression_parameters = ridge_regression.run_not_poisoned(config, instance_data)
-        unpoisoned_results["mse_final"].append(regression_parameters["mse"])
-        unpoisoned_results["solutions_dict"].append(regression_parameters)
-        unpoisoned_results.update({key : value for key, value in regression_parameters.items() if key not in unpoisoned_results})
-
-        # Run flipping attack
-        (
-            _,
-            _,
-            solutions,
-        ) = flipping_attack.run(config, instance_data, model)
-        # Run benchmark attack
-        config["iterative_attack_incremental"] = True
-        _, benchmark_data, benchmark_solution = numerical_attack.run(
-            config, instance_data
-        )
-        config["iterative_attack_incremental"] = False
-
         # Save results to dictionary
-        # Flipping algorithm results
-        flipping_results["mse_per_iteration"].append(solutions["mse_per_iteration"])
-        flipping_results["mse_final"].append(solutions["mse_final"])        
-        flipping_results["computational_time_per_iteration"].append(
-            solutions["computational_time_per_iteration"]
+        unpoisoned_results["mse_final"].append(regression_parameters["mse"])
+        unpoisoned_results["weights_num"].append(regression_parameters["weights_num"])
+        unpoisoned_results["weights_cat"].append(regression_parameters["weights_cat"])
+        unpoisoned_results["bias"].append(regression_parameters["bias"])
+
+        ## Flipping attack
+        _, _, flipping_solutions = flipping_attack.run(config, instance_data, model)
+        # Save results to dictionary
+        flipping_results["mse_per_iteration"].append(
+            flipping_solutions["mse_per_iteration"]
         )
+        flipping_results["mse_final"].append(flipping_solutions["mse_final"])
         flipping_results["computational_time_final"].append(
-            solutions["computational_time_final"]
+            flipping_solutions["computational_time_final"]
         )
-        benchmark_improvement = (
-            (solutions["mse"] - solutions["benchmark_mse_final"])
-            / solutions["benchmark_mse_final"]
-            * 100
-        )
-        ridge_improvement = (
-            (solutions["mse"] - regression_parameters["mse"])
-            / regression_parameters["mse"]
-            * 100
-        )
-        flipping_results["benchmark_improvement"].append(benchmark_improvement)
-        flipping_results["ridge_improvement"].append(ridge_improvement)
-        flipping_results["solutions_dict"].append(solutions)
-        flipping_results.update({key : value for key, value in solutions.items() if key not in flipping_results})
+        flipping_results["weights_num"].append(flipping_solutions["weights_num"])
+        flipping_results["weights_cat"].append(flipping_solutions["weights_cat"])
+        flipping_results["bias"].append(flipping_solutions["bias"])
+
+        ## Benchmark attack
+        config["iterative_attack_incremental"] = True
+        _, _, benchmark_solution = numerical_attack.run(config, instance_data)
+        config["iterative_attack_incremental"] = False
         # Benchmark results
-        benchmark_results["mse_final"].append(solutions["benchmark_mse_final"])
+        benchmark_results["mse_final"].append(benchmark_solution["mse"])
         benchmark_results["computational_time_final"].append(
-            solutions["benchmark_computational_time"]
+            benchmark_solution["computational_time"]
         )
-        benchmark_results["solutions_dict"].append(benchmark_solution)
-        benchmark_results.update({key : value for key, value in benchmark_solution.items() if key not in benchmark_results})
+        benchmark_results["weights_num"].append(benchmark_solution["weights_num"])
+        benchmark_results["weights_cat"].append(benchmark_solution["weights_cat"])
+        benchmark_results["bias"].append(benchmark_solution["bias"])
 
-        # # Run binary attack
-        # (
-        #     _,
-        #     _,
-        #     binery_solutions,
-        # ) = binary_attack.run(config, instance_data, model)
-
-    # Add average MSE to results
-    flipping_results["average_mse"] = np.mean(flipping_results["mse_final"])
-    flipping_results["average_computational_time"] = np.mean(
-        flipping_results["computational_time_final"]
-    )
-    flipping_results["benchmark_average_improvement"] = np.mean(
-        flipping_results["benchmark_improvement"]
-    )
-    benchmark_results["average_mse"] = np.mean(benchmark_results["mse_final"])
-    benchmark_results["average_computational_time"] = np.mean(
-        benchmark_results["computational_time_final"]
-    )
+        # # # Run binary attack
+        # _, _, binary_solutions = binary_attack.run(config, instance_data, model)
+        # # Save results to dictionary
+        # binary_results["mse_final"].append(binary_solutions["mse_final"])
+        # binary_results["computational_time_final"].append(
+        #     binary_solutions["computational_time_final"]
+        # )
+        # binary_results["weights_num"].append(binary_solutions["weights_num"])
+        # binary_results["weights_cat"].append(binary_solutions["weights_cat"])
+        # binary_results["bias"].append(binary_solutions["bias"])
 
     # Store numpy savez file
     np.savez(
@@ -151,16 +111,42 @@ def run(runs, config):
         f"results/{config['dataset_name']}/{folder_name}/unpoisoned_results.npz",
         **unpoisoned_results,
     )
+    np.savez(
+        f"results/{config['dataset_name']}/{folder_name}/binary_results.npz",
+        **binary_results,
+    )
 
+    # Store config as yaml file
+    with open(
+        f"results/{config['dataset_name']}/{folder_name}/config.yaml", "w"
+    ) as file:
+        documents = yaml.dump(config, file)
 
-# folder_name = f"{5}_BS{0.5}_TS{100}_PR{20}"
-# dictionary = f"results/{'5num5cat'}/{folder_name}"
-# flipping_results = np.load(f"{dictionary}/flipping_results.npz")
-# benchmark_results = np.load(f"{dictionary}/benchmark_results.npz")
-# print(flipping_results["mse_final"])
-# print(flipping_results["mse_per_iteration"])
-# print(flipping_results["computational_time_final"])
-# print(flipping_results["benchmark_improvement"])
-# print(flipping_results["ridge_improvement"])
-# print(benchmark_results["mse_final"])
-# print(benchmark_results["computational_time_final"])
+def choose_regularization(config, instance_data, possible_values):
+    """Function to choose regularization parameter
+    for ridge regression.
+
+    Parameters
+    ----------
+    config : dict
+        Dictionary containing configuration parameters.
+    instance_data : InstanceData
+        InstanceData object.
+    possible_values : list  
+        List of possible regularization parameters.
+
+    Returns
+    -------
+    float
+        Best regularization parameter.
+
+    """
+    regularization_results = []
+    for regularization in possible_values:
+        config["regularization"] = regularization
+        regression_parameters = ridge_regression.run_not_poisoned(config, instance_data)
+        regularization_results.append(regression_parameters["mse"])
+
+    print(regularization_results)
+    return max(regularization_results)
+
