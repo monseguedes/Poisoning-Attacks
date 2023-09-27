4f1c2a14f88091efe9983741e6b9e5f13dcf9b44
monseguedes <s1553571@ed.ac.uk>
2023-09-27 19:09:40 +0100

    Add computational experiments file for bilevel

--- programs/minlp/computational_experiments.py
+++ programs/minlp/computational_experiments.py
@@ -6,16 +6,24 @@ import itertools
 import yaml
 import flipping_attack
 import instance_data_class
+import copy
 
 if __name__ == "__main__":
     with open("programs/minlp/config.yml", "r") as config_file:
-        config = yaml.safe_load(config_file)
+        init_config = yaml.safe_load(config_file)
 
     seeds = [1, 2, 3, 4, 5]
-    poiscts = [12, 24, 36, 48, 60]
-    for seed, poisct in itertools.product(seeds, poiscts):
+    # poison_rates = [0.06, 0.08, 0.12, 0.16, 0.2]
+    poison_rates = [0.2]
+    batch_size = [1, 6, 'all']
+    config = copy.deepcopy(init_config)
+    for seed, rate in itertools.product(seeds, poison_rates):
+        # Update configs
+        config['seed'] = seed
+        config['poison_rate'] = rate
+        config['numerical_attack_mini_batch_size'] = 6
         instance_data = instance_data_class.InstanceData(
-        config, benchmark_data=True, seed=seed, poisoning_samples=poisct
+        config, benchmark_data=True, seed=seed
         )
         numerical_model = None
 
